738fcf15d51dd7630fc881db9bd29e79d52ada70
mochcharril <mochcharril2001@gmail.com>
2023-04-15 22:50:43 +0700

    check up tabel doktor

---
+++ app/Http/Controllers/Doctor/ReportController.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace App\Http\Controllers\Doctor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Patient;
+use App\Models\Medicine;
+use App\Models\IncomingMedicine;
+use App\Models\Payment;
+use PDF;
+
+class ReportController extends Controller
+{
+    private $param;
+    public function __construct(){
+        $this->middleware(['role:dokter']);
+    }
+
+    public function listCheckup(){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis', 'checkups.other_notes')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+            $this->param['getPatient'] = Patient::all();
+
+            return view('doctor.pages.report.checkups-list', $this->param);
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printCheckup(){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis', 'checkups.other_notes')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printCodeCheckup(Request $request){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis', 'checkups.other_notes')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->where('checkups.patient_id', $request->patient)
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printDateCheckup(Request $request){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->whereBetween('checkups.checkup_date', [$request->date_start, $request->date_end])
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+
+    public function listRecipe(){
+        try {
+            $this->param['getFilterRecipe'] = \DB::table('recipes')
+                                                    ->select('recipes.*')
+                                                    ->join('checkups', 'recipes.checkup_id', 'checkups.id')
+                                                    ->where('checkups.status_rm', '1')
+                                                    ->get();
+            $this->param['getRecipe'] = \DB::table('recipe_details')
+                                            ->select('recipes.date_recipe', 'recipes.code_rp', 'checkups.code_cu', 'patients.name as patient_name', 'users.name as doctor', 'medicines.name as medicine', 'recipe_details.medication_rules', 'recipe_details.qty', 'recipe_details.total')
+                                            ->join('recipes', 'recipe_details.recipe_id', 'recipes.id')
+                                            ->join('checkups', 'recipes.checkup_id', 'checkups.id')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->join('users', 'recipes.doctor_id', 'users.id')
+                                            ->join('medicines', 'recipe_details.medicine_id', 'medicines.id')
+                                            ->where('checkups.status_rm', '1')
+                                            ->orderBy('recipes.code_rp', 'asc')
+                                            ->get();
+
+            return view('doctor.pages.report.recipe-list', $this->param);
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printRecipe(){
+        try {
+            $this->param['getRecipe'] = \DB::table('recipe_details')
+                                            ->select('recipes.date_recipe', 'recipes.code_rp', 'checkups.code_cu', 'patients.name as patient_name', 'users.name as doctor', 'medicines.name as medicine', 'recipe_details.medication_rules', 'recipe_details.qty', 'recipe_details.total')
+                                            ->join('recipes', 'recipe_details.recipe_id', 'recipes.id')
+                                            ->join('checkups', 'recipes.checkup_id', 'checkups.id')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->join('users', 'recipes.doctor_id', 'users.id')
+                                            ->join('medicines', 'recipe_details.medicine_id', 'medicines.id')
+                                            ->where('checkups.status_rm', '1')
+                                            ->orderBy('recipes.code_rp', 'asc')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.recipe-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printCodeRecipe(Request $request){
+        try {
+            $this->param['getRecipe'] = \DB::table('recipe_details')
+                                            ->select('recipes.date_recipe', 'recipes.code_rp', 'checkups.code_cu', 'patients.name as patient_name', 'users.name as doctor', 'medicines.name as medicine', 'recipe_details.medication_rules', 'recipe_details.qty', 'recipe_details.total')
+                                            ->join('recipes', 'recipe_details.recipe_id', 'recipes.id')
+                                            ->join('checkups', 'recipes.checkup_id', 'checkups.id')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->join('users', 'recipes.doctor_id', 'users.id')
+                                            ->join('medicines', 'recipe_details.medicine_id', 'medicines.id')
+                                            ->where('checkups.status_rm', '1')
+                                            ->where('recipes.id', $request->recipe)
+                                            ->orderBy('recipes.code_rp', 'asc')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.recipe-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printDateRecipe(Request $request){
+        try {
+            $this->param['getRecipe'] = \DB::table('recipe_details')
+                                            ->select('recipes.date_recipe', 'recipes.code_rp', 'checkups.code_cu', 'patients.name as patient_name', 'users.name as doctor', 'medicines.name as medicine', 'recipe_details.medication_rules', 'recipe_details.qty', 'recipe_details.total')
+                                            ->join('recipes', 'recipe_details.recipe_id', 'recipes.id')
+                                            ->join('checkups', 'recipes.checkup_id', 'checkups.id')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->join('users', 'recipes.doctor_id', 'users.id')
+                                            ->join('medicines', 'recipe_details.medicine_id', 'medicines.id')
+                                            ->where('checkups.status_rm', '1')
+                                            ->whereBetween('recipes.date_recipe', [$request->date_start, $request->date_end])
+                                            ->orderBy('recipes.code_rp', 'asc')
+                                            ->get();
+
+            $pdf = PDF::loadview('doctor.pages.report.recipe-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+}
---
+++ app/Http/Controllers/Nurse/ReportController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Nurse;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Patient;
+use App\Models\Medicine;
+use PDF;
+
+class ReportController extends Controller
+{
+    private $param;
+    public function __construct(){
+        $this->middleware(['role:perawat']);
+    }
+
+    public function listCheckup(){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+            $this->param['getPatient'] = Patient::all();
+            
+            return view('nurse.pages.report.checkups-list', $this->param);
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printCheckup(){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('nurse.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printCodeCheckup(Request $request){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->where('checkups.patient_id', $request->patient)
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('nurse.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+    public function printDateCheckup(Request $request){
+        try {
+            $this->param['getCheckup'] = \DB::table('checkups')
+                                            ->select('checkups.checkup_date', 'checkups.id', 'patients.code_rm', 'checkups.code_cu', 'patients.name as patient_name', 'checkups.complaint', 'checkups.diagnosis')
+                                            ->join('patients', 'checkups.patient_id', 'patients.id')
+                                            ->whereBetween('checkups.checkup_date', [$request->date_start, $request->date_end])
+                                            ->get();
+
+            $this->param['getMeasureDetail'] = \DB::table('measure_patient_details')
+                                            ->select('measures.name', 'measure_patient_details.measure_patient_id', 'measure_patients.checkup_id')
+                                            ->join('measure_patients', 'measure_patient_details.measure_patient_id', 'measure_patients.id')
+                                            ->join('measures', 'measure_patient_details.measure_id', 'measures.id')
+                                            ->get();
+
+            $pdf = PDF::loadview('nurse.pages.report.checkups-print', $this->param)->setPaper('legal', 'landscape');
+            return $pdf->stream('cetak-laporan-pdf-obat-masuk', array("Attachment" => false));
+        } catch (\Exception $e) {
+            return redirect()->back()->withError($e->getMessage());
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect()->back()->withError('Terjadi kesalahan pada database', $e->getMessage());
+        }
+    }
+}
